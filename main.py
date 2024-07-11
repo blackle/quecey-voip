f68dfb89fd51ddcbcd175f879c7e60210ac5cc5f
blackle <isabelle@blackle-mori.com>
2024-07-11 16:17:43 -0400

    add assets for main call, also fix main.py

--- main.py
+++ main.py
@@ -2,27 +2,31 @@
 import asyncio
 from voip import runVoipClient, loadWAVtoPCM
 import phoneventure
-import fish
 import test
 import echo
 
 EXPERIMENTS = {
 	"1234": phoneventure.handler,
-	"1111": fish.handler,
 	"9999": echo.handler,
 	"0000": test.handler,
 }
 
+snd_enter_experiment = loadWAVtoPCM("assets/enter_experiment.wav")
+snd_invalid_experiment = loadWAVtoPCM("assets/invalid_experiment.wav")
+snd_teleport = loadWAVtoPCM("assets/teleport.wav")
+
 async def handler(call):
+	await call.playPCM(snd_enter_experiment)
+	await call.playTone(1000, .25)
 	try:
 		code = await asyncio.wait_for(call.getDTMF(filter="0123456789", n=4), 60)
 	except TimeoutError:
 		code = ""
 	if code in EXPERIMENTS:
-		await call.playPCM(loadWAVtoPCM("assets/teleport.wav"))
-		await EXPERIMENTS[code]
+		await call.playPCM(snd_teleport)
+		await EXPERIMENTS[code](call)
 	else:
-		await call.playPCM(loadWAVtoPCM("assets/invalid_experiment.wav"))
+		await call.playPCM(snd_invalid_experiment)
 
 if __name__ == "__main__":
 	runVoipClient(handler)
